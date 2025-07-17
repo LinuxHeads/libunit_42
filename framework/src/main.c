a524d7976783a68ad81d0cb8a09f9f6bac25cec8
Abdelrahman Salah <abdsalah@c2r4s5.42amman.com>
2025-07-17 20:11:50 +0300

    main function to test the loader

---
+++ framework/src/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 18:58:15 by abdsalah          #+#    #+#             */
+/*   Updated: 2025/07/17 20:06:13 by abdsalah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libunit.h"
+
+int f(void)
+{
+    printf("hi\n");
+
+    return (1);
+}
+
+int main(void)
+{
+    t_unit_test *list = NULL;
+
+    load_test(&list, "1", f);
+    load_test(&list, "2", f);
+    load_test(&list, "3", f);
+    load_test(&list, "4", f);
+    load_test(&list, "5", f);
+
+    printf("list head %s\n", list->test_name);
+    while(list->next)
+    {
+        list = list->next;
+    }
+    printf("list tail %s\n", list->test_name);
+    return (0);
+}
