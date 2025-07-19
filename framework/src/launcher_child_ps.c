7e465ef471557aea4b29a3e8882893c1207babf6
Abdelrahman Salah <abdsalah@c2r5s6.42amman.com>
2025-07-19 20:08:18 +0300

    remove redir from mandatory

--- framework/src/launcher_child_ps.c
+++ framework/src/launcher_child_ps.c
@@ -6,7 +6,7 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 19:11:10 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/07/19 19:56:01 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/07/19 20:07:48 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,6 @@ void	child_process(t_unit_test *test_node, t_unit_test *list)
 	{
 		ft_printf("Invalid test function.\n");
 		exit(EXIT_FAILURE);
-	}
-	devnull = open("/dev/null", O_WRONLY);
-	if (devnull >= 0)
-	{
-		dup2(devnull, STDOUT_FILENO);
-		dup2(devnull, STDERR_FILENO);
-		close(devnull);
 	}
 	alarm(TIMEOUT);
 	ret = test_node->f();
